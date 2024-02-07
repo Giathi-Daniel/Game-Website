f4ce17b70e5e011bdf1d05d6c8b226ac403c0f7c
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-07 11:20:11 +0300

    Updated Gamesite

---
+++ ps.js
@@ -0,0 +1,13 @@
+let game_bx_1 = document.getElementById("game_bx_1");
+let left_btn = document.getElementById("left_btn");
+let right_btn = document.getElementById("right_btn");
+
+left_btn.addEventListener('click', () => {
+    game_bx_1.scrollLeft -= 250;
+})
+
+right_btn.addEventListener('click', () => {
+    game_bx_1.scrollLeft += 250;
+})
+
+
--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let game_bx_1 = document.getElementById("game_bx_1");
-let left_btn = document.getElementById("left_btn");
-let right_btn = document.getElementById("right_btn");
-
-left_btn.addEventListener('click', () => {
-    game_bx_1.scrollLeft -= 250;
-})
-
-right_btn.addEventListener('click', () => {
-    game_bx_1.scrollLeft += 250;
-})
-
 let day_night = document.getElementById("night");
 let day_day = document.getElementById("day");
 
@@ -81,3 +69,118 @@ const wifi_change = () => {
 }
 setInterval(wifi_change, 100);
 wifi_change();
+
+
+// GAME DAOWNLOAD SCRIPT
+let img_change = document.getElementById('img_change');
+
+const change_img_data = () => {
+    setTimeout(() => {
+        img_change.style.background = "url('images/pubg.jpeg') no-repeat center center/cover";
+    }, 1000);
+    setTimeout(() => {
+        img_change.style.background = "url('images/pubg1.jpeg') no-repeat center center/cover";
+    }, 3000);
+    setTimeout(() => {
+        img_change.style.background = "url('images/pubg2.jpeg') no-repeat center center/cover";
+    }, 6000);
+}
+setInterval(change_img_data, 9000); //still have to change but it doesn't change anything
+change_img_data();
+
+
+// playing game
+let play = document.getElementById("play");
+let video = document.getElementById("video");
+
+play.addEventListener('click', () => {
+    if (video.paused || video.currentTime <= 0) {
+        video.play();
+        video.style.display = "flex";
+        img_change.style.display = "none";
+        play.innerHTML = `<i class="bi bi-pause-fill"></i> Pause`;
+    } else {
+        video.paused();
+        video.style.display = '';
+        img_change.style.display = '';
+        play.innerHTML = `<i class="bi bi-play-fill"></i> Play`;
+    }
+});
+
+video.addEventListener('ended', () => {
+    video.paused();
+    video.style.display = '';
+    img_change.style.display = '';
+    play.innerHTML = `<i class="bi bi-play-fill"></i> Play`;
+});
+
+const JoinData = [
+    {
+        name: "Blackshark",
+        game: "Grand Theft Auto V",
+        img: "images/user1.jpg",
+        price: "$800"
+    },
+    {
+        name: "Rachal Sindarera",
+        game: "Amazing Spider Man",
+        img: "images/user5.jpg",
+        price: "$332"
+    },
+    {
+        name: "Angel Angelina",
+        game: "Grand Theft Auto V",
+        img: "images/user4.jpg",
+        price: "$332"
+    },
+    {
+        name: "Steve Jobs",
+        game: "Sniper Ghost",
+        img: "images/user3.jpg",
+        price: "$832"
+    },
+    {
+        name: "Halima",
+        game: "Call Of Duty",
+        img: "images/user2.jpg",
+        price: "$232"
+    },
+    {
+        name: "Don MacDornalds",
+        game: "Ghost Recon Frontline",
+        img: "images/user6.jpg",
+        price: "$102"
+    },
+]
+
+let index = 0;
+
+let join_new = document.getElementsByClassName("join_new")[0];
+
+function update() {
+    setInterval(() => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        if (index == JoinData.length) {
+            index = 0;
+        } else {
+            index++;
+        }
+        const {name, game, img, price} = JoinData[index-1];
+        card.innerHTML = `<img src="${img}" alt="">
+        <div class="content">
+            <div class="price">
+                <h5>${game}</h5>
+                <span>Blackshark Now</span>
+                <h6>${price}</h6>
+            </div>
+            <p>${name}</p>
+        </div>`;
+        join_new.appendChild(card);
+        join_new.scrollTop += 100;
+    }, 1000);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    update();
+});
